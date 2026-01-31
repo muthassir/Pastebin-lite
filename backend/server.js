cad4bc6aa2bedca6f4ef762995d606d4b8fc74f0
muthassir <muthaseir@gmail.com>
2026-02-01 00:25:06 +0530

    changed index file path

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ const getNow = (req) => {
 app.use("/api/pastes", pasteRoutes);
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 app.get("/p/:id", async (req, res) => {
